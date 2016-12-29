25dbb0f682b87edfe1b3071851a3ab8f5c581c9c
MIDDLEEAST\a-muleit <a-muleit@microsoft.com>
2016-12-29 11:17:03 +0200

    Add console tester project

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azureoth.Modules.SQLdb;
+using Azureoth.Modules.SQLdb.Datastructures.Schema;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleTester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Please enter path to json file: ");
+            var path = Console.In.ReadLine();
+
+            string text = System.IO.File.ReadAllText(path);
+
+            var json = JsonConvert.DeserializeObject<Dictionary<string, JsonTable>>(text);
+
+            var translator = new SchemaTranslator();
+
+            var result = translator.JsonToSQL(json, "Hacking_Dev", "MySuperSecretSchema");
+
+            System.IO.File.WriteAllText(path.Replace(".json", ".sql"), result.RawSQL);
+        }
+    }
+}
